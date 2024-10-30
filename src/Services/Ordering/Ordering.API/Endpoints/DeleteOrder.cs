18734dbef96cf386d74cc52c129803d78da20579
Stuart Murray <stuart.murray.dev@outlook.com>
2024-10-30 18:33:21 +0000

    ordering API endpoints

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,24 @@
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
+
+// public record DeleteOrderRequest(Guid Id);
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(id));
+            var response = result.Adapt<DeleteOrderResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOrder")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Order")
+        .WithDescription("Delete Order");
+    }
+}
