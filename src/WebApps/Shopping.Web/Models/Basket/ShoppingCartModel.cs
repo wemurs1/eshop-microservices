eace444b9d77301944e0d0a195f478dd641e9415
Stuart Murray <stuart.murray.dev@outlook.com>
2024-11-08 15:59:11 +0000

    checkpoint webapp

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,23 @@
+namespace Shopping.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItemModel> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(x => x.Quantity * x.Price);
+}
+
+public class ShoppingCartItemModel
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
+    public Guid ProductId { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
+}
+
+// wrapper classes
+public record GetBasketResponse(ShoppingCartModel Cart);
+public record StoreBasketRequest(ShoppingCartModel Cart);
+public record StoreBasketResponse(string UserName);
+public record DeleteBasketresponse(bool IsSuccess);
---
+++ src/WebApps/Shopping.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,31 @@
+namespace Shopping.Web.Models.Ordering;
+
+public record OrderModel(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressModel ShippingAddress,
+    AddressModel BillingAddress,
+    PaymentModel Payment,
+    OrderStatus Status,
+    List<OrderItemModel> OrderItems
+);
+
+public enum OrderStatus
+{
+    Draft = 1,
+    Pending = 2,
+    Completed = 3,
+    Cancelled = 4
+}
+
+public record PaymentModel(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+
+public record AddressModel(string FirstName, string LastName, string EmailAddress, string AddressLine, string Country, string State, string ZipCode);
+
+public record OrderItemModel(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+
+// wrapper classes
+public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);
