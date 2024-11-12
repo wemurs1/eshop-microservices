256217d37cbb63832ec1db3bcb6c866a2da3cebe
Stuart Murray <stuart.murray.dev@outlook.com>
2024-11-12 13:05:58 +0000

    error page

---
+++ src/WebApps/Shopping.Web/Pages/Error.cshtml.cs
@@ -0,0 +1,14 @@
+namespace Shopping.Web.Pages
+{
+    public class ErrorModel : PageModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            RequestId = HttpContext.TraceIdentifier;
+        }
+    }
+}
